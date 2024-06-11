d405bf59e975c4c679770e65c98a607869a53159
focus110 <iamemmaochuko@gmail.com>
2024-06-11 05:49:38 +0100

    completed onboarding frontend

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
-import Topbar from "../components/shared/Topbar";
-import LeftSidebar from "../components/shared/LeftSidebar";
-import RightSidebar from "../components/shared/RightSidebar";
-import Bottombar from "../components/shared/Bottombar";
-import "../globals.css"
+import Topbar from "../../components/shared/Topbar";
+import LeftSidebar from "../../components/shared/LeftSidebar";
+import RightSidebar from "../../components/shared/RightSidebar";
+import Bottombar from "../../components/shared/Bottombar";
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +12,6 @@ export const metadata = {
   title: "Threads",
   description: "A social platform for communities",
 };
-
-
 
 export default function RootLayout({
   children,
